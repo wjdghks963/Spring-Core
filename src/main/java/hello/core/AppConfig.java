ca667964ed00c41750b382fd7b4c578abc6798fd
wjdghks963 <chsw000@naver.com>
2023-01-11 16:51:22 +0900

    Normal java code에서 Spring으로 변경하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,26 +9,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
 // Application의 동작에 필요한 구현 객체를 생성한다.
-
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         // 생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         // 생성자 주입
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
